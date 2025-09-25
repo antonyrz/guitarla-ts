e42c8a646631925213bd1034b3a51fe260933d06
AntonyHz <hernandezantony333@gmail.com>
2025-09-24 22:13:09 -0400

    agregando funcion add-to-cart al reducer

--- src/reducers/cart-reducer.ts
+++ src/reducers/cart-reducer.ts
@@ -8,10 +8,6 @@ export type CartActions =
     { type: 'increase-quantity', payload: {id: Guitar['id']}} |
     { type: 'clear-cart'};
 
-
-const MIN_ITEMS = 1
-const MAX_ITEMS = 5
-
 export type CartState = {
     data: Guitar[],
     cart: CartItem[]
@@ -22,6 +18,9 @@ export const initialState : CartState = {
     cart: [],
 };
 
+const MIN_ITEMS = 1
+const MAX_ITEMS = 5
+
 export const cartReducer = (
         state: CartState = initialState,
         action: CartActions
@@ -29,28 +28,40 @@ export const cartReducer = (
 
         if(action.type === 'add-to-cart'){
 
-            const newItem : Guitar = action.payload.item;
-            const actualCart = [...state.cart];
+            const ItemPayload = action.payload.item;
+            let updatedCart : CartItem[] = [];
 
-            let updatedCart;
-            
-            const itemExists = actualCart.findIndex(guitar => guitar.id === newItem.id)
+            const itemExists = state.cart.find(guitar => guitar.id === ItemPayload.id);
 
-            if(itemExists >= 0 ) { // existe en el carrito
+            console.log(itemExists);
 
-                if(actualCart[itemExists].quantity >= MAX_ITEMS) return
-                updatedCart = [...actualCart, actualCart[itemExists].quantity++]
+            if(itemExists){
+                // El item existe en el carrito
+                updatedCart = state.cart.map(item => {
 
-            } else {
-                const itemUpdated : CartItem = {...newItem, quantity : 1}
-                updatedCart = [...actualCart, itemUpdated]
+                    if(item.id === ItemPayload.id){
+
+                        if(item.quantity < MAX_ITEMS) return {...item, quantity: item.quantity++};
+
+                        // El item tiene mas de 5 en cantidad
+                        return item;
+
+                    }else{
+                        return item;
+                    }
+                });
+
+            }else{
+
+                // El item NO existe en el carrito
+                const newItem : CartItem = {...ItemPayload, quantity: 1};
+                updatedCart = [...state.cart, newItem]
             }
-
-
+                
             return{
                 ...state,
                 cart: updatedCart,
-            }
+            };
         }
 
         if(action.type === 'remove-from-cart'){
